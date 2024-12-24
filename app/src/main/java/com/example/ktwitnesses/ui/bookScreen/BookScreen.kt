6ddfebe9b1082c433eb5e3e0520d3bdb1463b9e5
TeeMur <timurs.kzn@gmail.com>
2024-12-24 21:03:46 +0300

    >showed this to Vladimir terekhin

--- app/src/main/java/com/example/ktwitnesses/ui/bookScreen/BookScreen.kt
+++ app/src/main/java/com/example/ktwitnesses/ui/bookScreen/BookScreen.kt
@@ -1,6 +1,7 @@
 package com.example.ktwitnesses.ui.bookScreen
 
 import CartViewModel
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
@@ -8,10 +9,12 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Icon
 //noinspection UsingMaterialAndMaterial3Libraries
@@ -23,6 +26,8 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.automirrored.outlined.ArrowForward
+import androidx.compose.material.icons.outlined.ArrowForward
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -34,6 +39,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -41,6 +47,7 @@ import coil.compose.AsyncImage
 import com.example.ktwitnesses.BooksUiState
 import com.example.ktwitnesses.HomeViewModel
 import com.example.ktwitnesses.R
+import com.example.ktwitnesses.ui.NavRoutes
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
@@ -71,43 +78,94 @@ fun BookScreen(
 			}
 		},
 		bottomBar = {
-			Row {
+			Row(
+				modifier = Modifier.padding(16.dp)
+			) {
 				val booksInCartCount = cartViewModel.cartItems.collectAsState()
 				if (booksInCartCount.value.getOrDefault(book, 0) == 0) {
-					Button(onClick = { cartViewModel.addToCart(book) },
-						modifier = Modifier.fillMaxWidth()) {
+					Button(
+						onClick = { cartViewModel.addToCart(book)},
+						shape = RoundedCornerShape(16.dp),
+						modifier = Modifier
+							.fillMaxWidth()
+							.height(56.dp),
+						elevation = ButtonDefaults.elevation(
+							defaultElevation = 0.dp
+						),
+						colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+						border = BorderStroke(2.dp, Color.Black)
+					) {
 						Row(
 							horizontalArrangement = Arrangement.SpaceBetween
 						) {
-							Text(text = stringResource(R.string.add_to_cart))
+							Text(text = stringResource(R.string.add_to_cart) + " ")
 							Text(text = book.price.toString() + stringResource(R.string.price_currency))
 						}
 					}
 				}
 				else {
 					Row(
-						horizontalArrangement = Arrangement.SpaceBetween,
 						verticalAlignment = Alignment.CenterVertically,
 						modifier = Modifier.fillMaxWidth()
 					) {
 						Button(
 							onClick = {cartViewModel.removeFromCart(book)},
+							shape = RoundedCornerShape(16.dp),
 							modifier = Modifier
-								.padding(horizontal = 16.dp, vertical = 8.dp)
+								.height(56.dp)
+								.weight(0.16f),
+							elevation = ButtonDefaults.elevation(
+								defaultElevation = 0.dp
+							),
+							colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+							border = BorderStroke(2.dp, Color.Black)
 						) {
 							Text("-")
 						}
 						Text(
+							modifier = Modifier.weight(0.16f),
 							text = "${booksInCartCount.value.getOrDefault(book, 0)}",
 							style = MaterialTheme.typography.body1,
-							color = Color.Gray
+							color = Color.Black,
+							fontSize = 24.sp,
+							textAlign = TextAlign.Center
 						)
 						Button(
 							onClick = {cartViewModel.addToCart(book)},
+							shape = RoundedCornerShape(16.dp),
 							modifier = Modifier
-								.padding(horizontal = 16.dp, vertical = 8.dp)
+								.height(56.dp)
+								.weight(0.16f),
+							elevation = ButtonDefaults.elevation(
+								defaultElevation = 0.dp
+							),
+							colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+							border = BorderStroke(2.dp, Color.Black)
 						) {
 							Text("+")
+						}
+						Button(
+							onClick = {navController.navigate(NavRoutes.Cart.route)},
+							shape = RoundedCornerShape(16.dp),
+							modifier = Modifier
+								.height(56.dp)
+								.weight(0.5f)
+								.padding(start = 4.dp),
+							elevation = ButtonDefaults.elevation(
+								defaultElevation = 0.dp
+							),
+							colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+							border = BorderStroke(2.dp, Color.Black),
+						) {
+							Row (
+								modifier = Modifier.fillMaxWidth()
+									.padding(horizontal = 4.dp),
+								horizontalArrangement = Arrangement.SpaceBetween,
+								verticalAlignment = Alignment.CenterVertically
+							) {
+								Icon(imageVector = Icons.AutoMirrored.Outlined.ArrowForward, contentDescription = "")
+								Text("Корзина")
+							}
 						}
 					}
 				}
@@ -122,8 +180,9 @@ fun BookScreen(
 		) {
 			AsyncImage(
 				modifier = Modifier
-					.fillMaxWidth()
 					.requiredHeight(240.dp)
+					.padding(vertical = 16.dp)
+
 					.clip(RoundedCornerShape(24.dp)),
 				model = coil.request.ImageRequest.Builder(context = LocalContext.current)
 					.data(book.imageLink?.replace("http", "https"))
